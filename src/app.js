9335e01a8ac17efa0416c2b9bf65da096cb40b99
CoolPenwin <coolPenwin@proton.me>
2024-07-24 21:03:01 +0000

    funciona raro el codespace

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+console.log("wazaaaaa");
 import "bootstrap";
 import "./style.css";
 
@@ -9,3 +10,62 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+// //  ♦ ♥ ♠ ♣
+// let paloArriba = document.querySelector("#simboloArriba");
+//   let numero = document.querySelector("#numero");
+//   let paloAbajo = document.querySelector("#simboloAbajo");
+//
+//
+// function asignarPalo(arr) {
+//     if (arr[0] === "corazon") {
+//       paloArriba.innerHTML = "♥";
+//       paloAbajo.innerHTML = "♥";
+
+let boton = getcard();
+getcard();
+
+function getcard() {
+  document.querySelector("#numero").innerHTML = numero();
+  switch (pinta()) {
+    case 1:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-heart"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-heart"></i>';
+      break;
+    case 2:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-diamond"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-diamond"></i>';
+      break;
+    case 3:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-clover"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-clover"></i>';
+      break;
+    case 4:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-rocket"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-rocket"></i>';
+      break;
+
+    default:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="<i class="fa-solid fa-wrench"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="<i class="fa-solid fa-rocket"></i>';
+      break;
+  }
+}
+
+function numero() {
+  return Math.ceil(Math.random() * 13);
+}
+
+function pinta() {
+  return Math.ceil(Math.random() * 4);
+}

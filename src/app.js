e8e161a243288d965e6cbe271ba2820a5cffaee1
CoolPenwin <coolPenwin@proton.me>
2024-07-25 13:41:27 +0000

    carta funcionando

--- src/app.js
+++ src/app.js
@@ -1,65 +1,57 @@
-/* eslint-disable */
 console.log("wazaaaaa");
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/4geeks.ico";
-// import "https://trucosdemagia.net/wp-content/uploads/2018/10/tapetes-magia.jpg";
 
-let titulo = "Magic trik"; // Definir la variable
-document.title = titulo; // Asignar el valor de la variable al título de la pági
+let titulo = "Magic trik";
+document.title = titulo;
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
   getcard();
 };
 
-// //  ♦ ♥ ♠ ♣
-// let paloArriba = document.querySelector("#simboloArriba");
-//   let numero = document.querySelector("#numero");
-//   let paloAbajo = document.querySelector("#simboloAbajo");
-//
-//
-// function asignarPalo(arr) {
-//     if (arr[0] === "corazon") {
-//       paloArriba.innerHTML = "♥";
-//       paloAbajo.innerHTML = "♥";
+function valorRandom() {
+  let numero = Math.ceil(Math.random() * 13);
 
-let boton = getcard();
-getcard();
-
-function numero() {
-  return Math.ceil(Math.random() * 13);
+  if (numero == 1) {
+    numero = "A";
+  } else if (numero == 11) {
+    numero = "J";
+  } else if (numero == 12) {
+    numero = "Q";
+  } else if (numero == 13) {
+    numero = "K";
+  }
+  return numero;
 }
 
-function pinta() {
+function paloRandom() {
   return Math.ceil(Math.random() * 4);
 }
 
 function getcard() {
-  document.querySelector("#numero").innerHTML = numero();
-  switch (pinta()) {
+  document.querySelector("#numero").innerHTML = valorRandom();
+  switch (paloRandom()) {
     case 1:
-      document.querySelector("#pinta1").innerHTML = "♦";
-      document.querySelector("#pinta2").innerHTML = "♦";
+      document.querySelector("#carta").classList.add("diamante");
+      document.querySelector("#paloTop").innerHTML = "♦";
+      document.querySelector("#paloBot").innerHTML = "♦";
       break;
     case 2:
-      document.querySelector("#pinta1").innerHTML = "♥";
-      document.querySelector("#pinta2").innerHTML = "♥";
+      document.querySelector("#carta").classList.add("corazon");
+      document.querySelector("#paloTop").innerHTML = "♥";
+      document.querySelector("#paloBot").innerHTML = "♥";
       break;
     case 3:
-      document.querySelector("#pinta1").innerHTML = "♠";
-      document.querySelector("#pinta2").innerHTML = "♠";
+      document.querySelector("#carta").classList.add("pica");
+      document.querySelector("#paloTop").innerHTML = "♠";
+      document.querySelector("#paloBot").innerHTML = "♠";
       break;
     case 4:
-      document.querySelector("#pinta1").innerHTML = "♣";
-      document.querySelector("#pinta2").innerHTML = "♣";
-      break;
-
-    default:
-      document.querySelector("#pinta1").innerHTML = "♣";
-      document.querySelector("#pinta2").innerHTML = "♥";
+      document.querySelector("#carta").classList.add("trebol");
+      document.querySelector("#paloTop").innerHTML = "♣";
+      document.querySelector("#paloBot").innerHTML = "♣";
       break;
   }
 }
